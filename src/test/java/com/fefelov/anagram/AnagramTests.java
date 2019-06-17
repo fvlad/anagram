3191b7ecd1d2e100f5a6cb2845029365bcc40c4c
Vladislav Fefelov <vladislavf@playtika.com>
2019-06-17 20:01:21 +0300

    Add test

--- src/test/java/com/fefelov/anagram/AnagramTests.java
+++ src/test/java/com/fefelov/anagram/AnagramTests.java
@@ -3,17 +3,26 @@ package com.fefelov.anagram;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class AnagramTests {
 
-    private Anagram anagram = new Anagram(true);
+    private Anagram anagram = new Anagram();
 
     @Test
     public void testAnagrams() throws Exception {
+        assertEquals(
+            Arrays.asList("abc", "BAc", "bca", "Bac"),
+            anagram.collectAnagrams(Arrays.asList("abc", "Aaa", "aaa", "BAc", "bca", "aba", "Bac").toArray(new String[0]), "abc"));
+    }
+
+    @Test
+    public void testDictionary() throws Exception {
         final String path = Anagram.class.getClassLoader().getResource("pldf-win.txt").getPath();
         final String[] dictionary = Files.readAllLines(Paths.get(path), Charset.forName("CP1251"))
             .toArray(new String[0]);
